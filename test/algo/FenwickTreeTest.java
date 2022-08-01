1c4b6c2db415de10631e3aab507cccc31a1b8d80
Aleksandr Nikiforov <alex@alexn.ee>
2022-08-01 00:28:01 -0400

    FenwickTree test

---
+++ test/algo/FenwickTreeTest.java
@@ -0,0 +1,35 @@
+package algo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class FenwickTreeTest {
+
+	@Test
+	public void test() {
+		
+		FenwickTree fw = new FenwickTree(new int[] { 1, 2, 3, 4, 5} );
+		assertEquals(15, fw.sum(0, 4));
+		assertEquals(1, fw.sum(0, 0));
+		assertEquals(5, fw.sum(4, 4));
+		
+		fw.inc(0, 100);
+		
+		assertEquals(115, fw.sum(0, 4));
+		assertEquals(101, fw.sum(0, 0));
+		assertEquals(5, fw.sum(4, 4));
+		
+		fw.inc(0, -50);
+		
+		assertEquals(65, fw.sum(0, 4));
+		assertEquals(51, fw.sum(0, 0));
+		assertEquals(5, fw.sum(4, 4));
+		
+		fw.inc(4, 10);
+		
+		assertEquals(15, fw.sum(4, 4));
+		
+	}
+	
+}
